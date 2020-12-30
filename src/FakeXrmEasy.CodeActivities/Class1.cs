1c86c5d5a69995d275b83181f4f46f8801ab350d
jordimontana82 <jordi.montana@gmail.com>
2020-12-30 15:36:01 +0100

    Added initial project structure

---
+++ src/FakeXrmEasy.CodeActivities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FakeXrmEasy.CodeActivities
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/FakeXrmEasy.CodeActivities.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FakeXrmEasy.CodeActivities.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
